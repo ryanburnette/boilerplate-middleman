6645b737163730260f7ab2dfabfb1fbe65d7f2de
Ryan Burnette <ryan.burnette@gmail.com>
2017-02-19 16:10:07 -0500

    sitemap.xml fix #3

--- helpers/sitemap_helpers.rb
+++ helpers/sitemap_helpers.rb
@@ -1,11 +1,22 @@
 module SitemapHelpers
+  def sitemap_path(resource)
+    "#{canonical_url_prefix}/#{resource.destination_path}"
+  end
+
   def sitemap_resources
-    sitemap.resources.
-      reject { |r| r.destination_path.include?("sitemap.xml") }.
-      reject { |r| r.destination_path.end_with?(".css") }.
-      reject { |r| r.destination_path.end_with?(".png") }.
-      reject { |r| r.destination_path.end_with?(".js") }.
-      reject { |r| r.destination_path.end_with?("robots.txt") }
+    sitemap.resources
+      .reject { |r| r.destination_path.include?("sitemap.xml") }
+      .reject { |r| r.destination_path.end_with?(".css") }
+      .reject { |r| r.destination_path.end_with?(".png") }
+      .reject { |r| r.destination_path.end_with?(".js") }
+      .reject { |r| r.destination_path.end_with?(".eot") }
+      .reject { |r| r.destination_path.end_with?(".otf") }
+      .reject { |r| r.destination_path.end_with?(".gif") }
+      .reject { |r| r.destination_path.end_with?(".svg") }
+      .reject { |r| r.destination_path.end_with?(".ttf") }
+      .reject { |r| r.destination_path.end_with?(".woff") }
+      .reject { |r| r.destination_path.end_with?(".woff2") }
+      .reject { |r| r.destination_path.include?("robots.txt") }
   end
 
   def sitemap_priority(resource)
